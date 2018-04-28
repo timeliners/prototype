1d7440dfa6a8ae994ead70ab46be1e91077d9a3c
Cedric van Putten <me@bycedric.com>
2018-04-28 17:31:41 +0200

    feature: add Enzyme and Chai for testing
    With Chai we have an improved assertion library with more capabilities than Jest itself. Also with Enzyme we have more control about test-rendering of the component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+
 import App from './App';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
-	ReactDOM.unmountComponentAtNode(div);
+describe('Component: App', () => {
+	it('renders without crashing', () => {
+		expect(shallow(<App />).html()).to.be.ok;
+	});
 });
