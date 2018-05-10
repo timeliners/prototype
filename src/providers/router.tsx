b124742bedab374d63b25f841565c789c84c49ce
Cedric van Putten <me@bycedric.com>
2018-05-10 18:39:42 +0200

    feature: add new modules to the router

--- src/providers/router.tsx
+++ src/providers/router.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import GithubSearch from '../modules/github-search';
 import GithubEvents from '../modules/github-events';
 
 export default function Router() {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route exact path='/' component={GithubEvents} />
+				<Route path='/:username' component={GithubEvents} />
+				<Route exact path='/' component={GithubSearch} />
 			</Switch>
 		</BrowserRouter>
 	);
