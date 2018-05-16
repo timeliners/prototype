2f5952533c69e90dd2459e165997897191ba76a6
joyceprive <jcfalling3@gmail.com>
2018-05-16 16:08:50 +0200

    Adds h1, h2, h3-styling for Card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -4,7 +4,10 @@ import './card.css';
 export default function Card() {
 	return (
 		<div className="Card">
-		Card example 1
+			<h1>PushEvent</h1>
+			<h2>Timeliners/prototype</h2>
+			<p>started <br/>
+			16 May 2018 10:45:43</p>
 		</div>
 	);
 }
