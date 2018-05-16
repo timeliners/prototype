54e158cfd8ee3ad8913de358df4224f1509de58c
joyceprive <jcfalling3@gmail.com>
2018-05-16 14:18:54 +0200

    Deletes unused standard text from App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -6,13 +6,6 @@ import logo from '../../logo.svg';
 export default function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1 className="App-title">Welcome to React</h1>
-			</header>
-			<p className="App-intro">
-				To get started, edit <code>src/App.tsx</code> and save to reload.
-			</p>
 			<img src={logo} className="Profilepicture" alt="username" />
 			<Card />
 		</div>
