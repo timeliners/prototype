81706fa58979d728828b58a9063c897a0f0cd105
Cedric van Putten <me@bycedric.com>
2018-06-25 22:20:27 +0200

    fix: remove test code for card component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import './app.css';
-import Card from '../card';
 import logo from '../../logo.svg';
 
 export default function App() {
 	return (
 		<div className="App">
 			<img src={logo} className="Profilepicture" alt="username" />
-			<Card />
 		</div>
 	);
 }
