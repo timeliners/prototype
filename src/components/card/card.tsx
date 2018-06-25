c4681c8935e6d6dc18e438da3a623bd7ee5165e1
Cedric van Putten <me@bycedric.com>
2018-06-25 22:28:26 +0200

    feature: add card timestamp and title parsing

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,5 +1,20 @@
 import * as React from 'react';
+import moment from 'moment';
 import './card.css';
+
+export function formatTimestamp(timestamp: string) {
+	return moment(timestamp).format('YYYY-MM-DD');
+}
+
+export const titles = {
+	CreateEvent: 'Created branch',
+	DeleteEvent: 'Deleted branch',
+	IssueCommentEvent: 'Commented',
+	IssuesEvent: 'Created issue',
+	PullRequestEvent: 'Request changes',
+	PushEvent: 'Pushed update',
+	ReleaseEvent: 'Released version',
+};
 
 export interface IProps {
 	title: string;
@@ -10,12 +25,9 @@ export interface IProps {
 export default function Card(props: IProps) {
 	return (
 		<div className="Card">
-			<h1>{props.title}</h1>
+			<h1>{titles[props.title] || props.title}</h1>
 			<h2>{props.subtitle}</h2>
-			<p>{props.timestamp}</p>
+			<p>{formatTimestamp(props.timestamp)}</p>
 		</div>
 	);
 }
-
-
-
