1c8e2e93427cdaf92c84ffc5a0fc425969fd4522
Cedric van Putten <me@bycedric.com>
2018-07-03 22:25:11 +0200

    test: expand card component test
    This includes classname checks and time element datetime property.

--- src/components/card/card.test.tsx
+++ src/components/card/card.test.tsx
@@ -13,9 +13,23 @@ describe('components/card/card', () => {
 			/>
 		);
 
-		component.contains('Interesting title');
-		component.contains('Awesome subtitle');
-		component.contains('now');
+		expect(component.contains('Interesting title')).toBeTruthy();
+		expect(component.contains('Awesome subtitle')).toBeTruthy();
+		expect(component.contains('now')).toBeTruthy();
+	});
+
+	it('renders title, subtitle and timestamp with proper class names', () => {
+		const component = shallow(
+			<Card
+				title='Interesting title'
+				subtitle='Awesome subtitle'
+				timestamp='now'
+			/>
+		);
+
+		expect(component.find('h1').hasClass('card__title')).toBeTruthy();
+		expect(component.find('h2').hasClass('card__subtitle')).toBeTruthy();
+		expect(component.find('time').hasClass('card__timestamp')).toBeTruthy();
 	});
 
 	it('renders formatted timestamp with iso timestmaps', () => {
@@ -27,6 +41,7 @@ describe('components/card/card', () => {
 			/>
 		);
 
-		component.contains('2018-06-27');
+		expect(component.contains('2018-06-27')).toBeTruthy();
+		expect(component.find('time').prop('dateTime')).toBe('2018-06-27T00:04:09.075Z');
 	});
 });
